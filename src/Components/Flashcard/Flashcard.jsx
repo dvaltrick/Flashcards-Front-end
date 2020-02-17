53de53f093baa73b686f7155f419cc6f9876845d
Diego Ecker Valtrick <diego.valtrick@softexpert.com>
2020-02-17 17:07:31 -0300

    melhoria

--- src/Components/Flashcard/Flashcard.jsx
+++ src/Components/Flashcard/Flashcard.jsx
@@ -2,20 +2,41 @@ import React, { Component } from "react";
 import {Paper} from "@material-ui/core";
 import PropTypes from "prop-types";
 
+import classes from "./Flashcard.css";
+
 class Flashcard extends Component {
     render() {
         return (
-            <Paper elevation={3}
-                style={{
-                    width: this.props.width + "px",
-                    height: this.props.height + "px",
-                    backgroundColor: this.props.backgroundColor,
-                    color: "white",
-                    padding: '20px',
-                    margin: this.props.margin + 'px'
-                }}>
-                    {this.props.children}
-            </Paper>
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <Paper elevation={3}
+                            style={{
+                                width: this.props.width + "px",
+                                height: this.props.height + "px",
+                                backgroundColor: this.props.backgroundColor,
+                                color: "white",
+                                padding: '20px',
+                                margin: this.props.margin + 'px'
+                            }}>
+                                {this.props.children}
+                        </Paper>
+                    </div>
+                    <div class="flip-card-back">
+                        <Paper elevation={3}
+                            style={{
+                                width: this.props.width + "px",
+                                height: this.props.height + "px",
+                                backgroundColor: "white",
+                                color: "black",
+                                padding: '20px',
+                                margin: this.props.margin + 'px'
+                            }}>
+                                {this.props.children}
+                        </Paper>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
